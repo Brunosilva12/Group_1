65e307cf7fa55754607edc1f36aad2a66020dbbb
erikadasilva <erika.dasilva@unine.ch>
2020-11-04 09:28:00 +0100

    starting new code

---
+++ Loco-vid.py
@@ -0,0 +1,8 @@
+#import pygame, random, sys
+#from pygame.locals import *
+
+WINDOWWIDTH = 600 # Taille de l'écran
+WINDOWHEIGHT = 600
+TEXTCOLOR = (0, 0, 0) # Couleur du text
+BACKGROUNDCOLOR = (255, 255, 255)  # Couleur du fond
+FPS = 60            # Nombre d'image par secondes
