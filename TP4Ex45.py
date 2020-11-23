425fb7fc58f0ad5b05b7bac0732303fc385f1253
tiffany <tiffany.garcia@unine.ch>
2020-11-23 15:08:25 +0100

    Pour créer une fenetre vide

--- TP4Ex45.py
+++ TP4Ex45.py
@@ -1,13 +1,24 @@
 import pygame
 from pygame.locals import *
 
+#Lancer pygame
 pygame.init()
+#Ouvrir une fenêtre de la même taille que le jeu
 fenetre = pygame.display.set_mode((1000,600))
-continuer = 1
+running = 1
 
-while continuer :
+#Chargement image
+menu = pygame.image.load("menu 1.jpg").convert()
+fenetre.blit(menu, (0,0))
+pygame.display.flip()
+
+#Boucle perpétuelle qui permet de garder la fenêtre ouverte
+while running :
     for event in pygame.event.get():
         if event.type == QUIT :
             continuer = 0
             pygame.quit()
             exit()
+
+
+
